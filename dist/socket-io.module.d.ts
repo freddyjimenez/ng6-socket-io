a7fb2f8633dea16f461f91e8a6fd002db0a7afed
Freddy Jimenez <freddy.jimenez@smartsense.io>
2019-01-16 15:43:22 -0300

    new version

--- dist/socket-io.module.d.ts
+++ dist/socket-io.module.d.ts
@@ -1,10 +1,9 @@
 import { InjectionToken, ModuleWithProviders, NgZone } from '@angular/core';
 import { WrappedSocket } from './socket-io.service';
 import { SocketIoConfig } from './socketIoConfig';
-
 /** Socket factory */
 export declare function SocketFactory(config: SocketIoConfig, ngZone: NgZone): WrappedSocket;
 export declare const SOCKET_CONFIG_TOKEN: InjectionToken<SocketIoConfig>;
 export declare class SocketIoModule {
-  static forRoot(config: SocketIoConfig): ModuleWithProviders;
+    static forRoot(config: SocketIoConfig): ModuleWithProviders;
 }
